407df540b2daa1c3ad094d8c1064670183a2ca69
binmuji <kap.binmuji@gmail.com>
2022-03-01 21:27:49 +0700

    perulangan for

---
+++ python02.py
@@ -0,0 +1,14 @@
+'''
+perulangan dengan for
+'''
+
+jumlah_buku = 10
+print ('bacalah 10 buku tugas')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print (f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+for jumlah_buku_yang_sudah_dibaca in range (1, jumlah_buku+1):
+    print (f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+
+print (f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
