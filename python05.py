b7debbca0e6bfe6821fdb52d1f02923b3ddb996a
binmuji <kap.binmuji@gmail.com>
2022-03-02 17:03:22 +0700

    data list 1

--- python05.py
+++ python05.py
@@ -14,4 +14,13 @@ print ('\ntampilkan isi daftar_buku dgn indeks')
 print (daftar_buku[0])
 print (daftar_buku[2])
 
+print ('\ntampilkan dengan for in range')
+for i in range (0, len(daftar_buku)):
+    print (daftar_buku[i])
+
+daftar_buku=[1, 'mangaku', -33, 3.14]
+print ('\ntampilkan dgn for in range, dimana tipe data berbeda')
+for i in range (0, len(daftar_buku)):
+    print (daftar_buku[i])
+
 
