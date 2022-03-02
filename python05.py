3b4be6c1043e462d46ddc4f03dc46f3c53ee432a
binmuji <kap.binmuji@gmail.com>
2022-03-02 11:44:14 +0700

    tipe data list

---
+++ python05.py
@@ -0,0 +1,17 @@
+'''
+penggunaan tipe data list
+'''
+
+daftar_buku = ['seven habits', 'how to influence people', 'first things first']
+print ('\ntampilkan variable daftar_buku')
+print (daftar_buku)
+
+print ('\nproses semua dgn for in')
+for buku in daftar_buku :
+    print (buku)
+
+print ('\ntampilkan isi daftar_buku dgn indeks')
+print (daftar_buku[0])
+print (daftar_buku[2])
+
+
