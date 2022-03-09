039d7e14deef6e4ba9a6658d69e21236430b901b
binmuji <kap.binmuji@gmail.com>
2022-03-10 06:24:11 +0700

    py dict

---
+++ python06.py
@@ -0,0 +1,18 @@
+'''
+python dictionary = json yang berfungsi utk pembuatan REST API 
+referensi data https://jsonplaceholder.typicode.com/users
+'''
+
+from xml.dom import UserDataHandler
+
+
+users = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    
+}
+print (users)
+print (users["id"]) #panggil dgn key
+
