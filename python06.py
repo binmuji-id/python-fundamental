a31ea3e994e60be3132c4bf769933296aa26d41d
binmuji <kap.binmuji@gmail.com>
2022-03-19 05:02:57 +0700

    create json file

--- python06.py
+++ python06.py
@@ -11,11 +11,31 @@ users = {
     "name": "Leanne Graham",
     "username": "Bret",
     "email": "Sincere@april.biz",
-    
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        
+            
+    }
 }
 print (users)
+print ("\n")
 print (users["id"]) #panggil dgn key
 print (users["name"])
 print (users["username"])
 print (users["email"])
+print (users["address"])
+print (users["address"]["zipcode"])
 
+print("\n dari dict ke json")
+print (users)
+print (type(users))
+import json #panggil package json
+result = json.dumps(users) #menjadikan string
+print (result)
+print (type(result))
+
+with open ("result.json","w") as file : #membuat file
+    json.dump (users, file)
